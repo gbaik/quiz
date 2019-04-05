110bc22cab08aaebd522be1426199543aaa36d16
Gideon Baik <gideonbaik@gmail.com>
2019-04-05 00:49:28 -0700

    Init (add basic steps for part 1 of exercise)

---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+
+- Return next question in CSV right immediately
+- Parse CSV file (problems.csv) and return on command
+- Take input from user
+- At end of quiz return how many correct questions 
+	- Invalid answers = incorrect
+- Be able to take flag for the filename
+
+*/
